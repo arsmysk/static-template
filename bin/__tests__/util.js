18217b767a22c8e5ab64106aebafc280d85796b8
sundaycrafts <sundaycrafts@users.noreply.github.com>
2018-02-27 15:11:56 +0900

    ensure distPath() util return path when passed sub directory path

--- bin/__tests__/util.js
+++ bin/__tests__/util.js
@@ -1,33 +1,34 @@
 const {distPath} = require('../util')
+const {dist, src} = require('../../config')
 
 describe('distPath', () => {
   it('change ext', () => {
-    const path = distPath('/src/path/to/file.js', '.php')
-    expect(path).toEqual('/dist/path/to/file.php')
+    const path = distPath(`${src}/path/to/file.js`, '.php')
+    expect(path).toEqual(`${dist}/path/to/file.php`)
   })
 
   it('path start from `/src`', () => {
-    const path = distPath('/src/path/to/file.js')
-    expect(path).toEqual('/dist/path/to/file.js')
+    const path = distPath(`${src}/path/to/file.js`)
+    expect(path).toEqual(`${dist}/path/to/file.js`)
   })
 
   it('path NOT start from `/src`', () => {
-    const path = distPath('/somewhere/not/start/src/file.js')
-    expect(path).toEqual('/somewhere/not/start/dist/file.js')
+    const path = distPath(`/somewhere/not/start/${src}/file.js`)
+    expect(path).toEqual(`/somewhere/not/start/${dist}/file.js`)
   })
 
-  it('path NOT start from `/`', () => {
-    const path = distPath('not/start/slash/src/file.js')
-    expect(path).toEqual('not/start/slash/dist/file.js')
+  it('path start from `/`', () => {
+    const path = distPath(`not/start/slash/${src}/file.js`)
+    expect(path).toEqual(`not/start/slash/${dist}/file.js`)
   })
 
   it('path start from `./`', () => {
-    const path = distPath('./start/dotslash/src/file.js')
-    expect(path).toEqual('./start/dotslash/dist/file.js')
+    const path = distPath(`./start/dotslash/${src}/file.js`)
+    expect(path).toEqual(`./start/dotslash/${dist}/file.js`)
   })
 
   it('path NOT include `src`', () => {
-    const path = distPath('./not/include/s_c/file.js')
-    expect(path).toEqual('./not/include/s_c/file.js')
+    const path = distPath(`./not/include/s_c/file.js`)
+    expect(path).toEqual(`./not/include/s_c/file.js`)
   })
 })
--- bin/util.js
+++ bin/util.js
@@ -4,21 +4,8 @@ const glob = require('globby')
 const config = require('../config')
 
 exports.distPath = (filePath, ext = path.extname(filePath)) => {
-  const distPath = filePath.split('/')
-    .reduce((acc, piece) => {
-      let newPiece, newIsReplaced
-
-      if (!acc.isReplaced && piece === config.src) {
-        newPiece = config.dist
-        newIsReplaced = true
-      } else {
-        newPiece = piece
-        newIsReplaced = acc.isReplaced
-      }
-
-      return {isReplaced: newIsReplaced, pathes: [...acc.pathes, newPiece]}
-    }, {isReplaced: false, pathes: []})
-    .pathes.join('/')
+  const pattern = new RegExp(config.src.replace(/\//g, '\\/'))
+  const distPath = filePath.replace(pattern, config.dist)
 
   return path.format({...path.parse(distPath),
     name: path.parse(distPath).name,
