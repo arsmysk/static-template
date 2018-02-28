2899a9f98745453e48f06929af783ec0aad8d50c
sundaycrafts <sundaycrafts@users.noreply.github.com>
2018-02-28 13:35:15 +0900

    Install flowtype & eslint

--- src/assets/script/top.js
+++ src/assets/script/top.js
@@ -1,35 +1,51 @@
+// @flow
 import Vue from 'vue'
 import App from 'App'
 
-import deps from 'mods/deps'
+import deps from "mods/deps"
 
 /** Reffering pcocess.env example */
 console.log('NODE_ENV: ', process.env.NODE_ENV)
-console.log('ACCESS_TOKEN from .env: ', process.env.ACCESS_TOKEN ||
-  'You can add .*.env file in project root to reffer process.env entry from js')
+console.log(
+  'ACCESS_TOKEN from .env: ',
+  process.env.ACCESS_TOKEN ||
+    'You can add .*.env file in project root to reffer process.env entry from js',
+)
 
 /** Simply import example */
-deps('Hello!')
+deps("Hello!")
 
 /** Polyfill example(Promise, setImmediate) */
-Promise.resolve().then(() => setImmediate(() => console.log('Promise() Polyfill')))
+Promise.resolve().then(() =>
+  setImmediate(() => console.log('Promise() Polyfill')),
+)
 
 /** ES2015+ syntax example */
 const asyncAwait = async () => {
-  const data = await new Promise(resolve => setTimeout(() => {
-    resolve('Async data')
-  }, 1000))
+  const data = await new Promise(resolve =>
+    setTimeout(() => {
+      resolve('Async data')
+    }, 1000),
+  )
   console.log('ES2015+ syntax: ', data)
 }
 asyncAwait()
 
 /** Vue.js example */
-const initVue = () => new Vue({
-    render: h => h(App)
+const initVue = () =>
+  new Vue({
+    render: h => h(App),
   }).$mount('#app')
 
-if (document.readyState !== 'loading') {
+if (document.readyState !== "loading") {
   initVue()
 } else {
-  document.addEventListener('DOMContentLoaded', initVue)
+  document.addEventListener("DOMContentLoaded", initVue)
 }
+
+/** flowtype example */
+const checkFlow = (x: number): string => {
+  return `${x} is number as string`
+}
+
+checkFlow(10)
