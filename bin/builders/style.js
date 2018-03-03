3c07e990826e239dc526072118bbb1128965e8e0
sundaycrafts <sundaycrafts@users.noreply.github.com>
2018-03-04 00:01:20 +0900

    add style calc() plugin

--- bin/builders/style.js
+++ bin/builders/style.js
@@ -7,7 +7,8 @@ const autoPrefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const mqpacker = require('css-mqpacker')
 const presetEnv = require('postcss-preset-env')
-const partialImport = require("postcss-partial-import")
+const partialImport = require('postcss-partial-import')
+const calc = require('postcss-calc')
 
 const {distPath} = require('../util')
 const config = require('../../config')
@@ -22,35 +23,37 @@ const commonPlugins = [
     prefix: '_',
   }),
   presetEnv({
-    stage: 0
+    stage: 0,
   }),
+  calc(),
   cssModules({
     generateScopedName: '[name]_[local]_[hash:base64:5]',
 
-    getJSON (cssFileName, json, outputFileName) {
+    getJSON(cssFileName, json, outputFileName) {
       const nameSpace = camelcase(path.parse(cssFileName).name)
-      const cameledJson = Object.entries(json).reduce((acc, [key, val]) => ({
-        ...acc,
-        [camelcase(key)]: val
-      }), {})
+      const cameledJson = Object.entries(json).reduce(
+        (acc, [key, val]) => ({
+          ...acc,
+          [camelcase(key)]: val,
+        }),
+        {},
+      )
 
       store.dispatch(addClassNames({[nameSpace]: cameledJson}))
-    }
+    },
   }),
 ]
 
-const productionPlugins = [
-  autoPrefixer,
-  mqpacker,
-  cssnano,
-]
+const productionPlugins = [autoPrefixer, mqpacker, cssnano]
 
-const usingPlugins = dev ? commonPlugins :
-  [...commonPlugins,
-    ...productionPlugins,
-  ]
+const usingPlugins = dev
+  ? commonPlugins
+  : [...commonPlugins, ...productionPlugins]
 
 module.exports = (store, {file, content}) => async dist => {
-  const builded = await postcss(usingPlugins).process(content, {from: file, to: dist})
+  const builded = await postcss(usingPlugins).process(content, {
+    from: file,
+    to: dist,
+  })
   return {file: dist, content: builded}
 }
