03a76ed8928fba08250264ba8112da3fb6c4c139
sundaycrafts <sundaycrafts@users.noreply.github.com>
2018-03-05 11:04:25 +0900

    inject env / common json data to template

--- bin/builders/template.js
+++ bin/builders/template.js
@@ -9,16 +9,23 @@ nunjucks.configure(path.join(process.cwd(), 'src'), {
 })
 
 beautifyConfig = {
-  'indent_size': 2,
-  'end_with_newline': true,
-  'preserve_newlines': true,
-  'max_preserve_newlines': 1,
-  'wrap_line_length': 120,
+  indent_size: 2,
+  end_with_newline: true,
+  preserve_newlines: true,
+  max_preserve_newlines: 1,
+  wrap_line_length: 120,
 }
 
 module.exports = ({style: {classNames}, data: {entities}}, {file, content}) => {
   const page = path.parse(file).name
-  const builded = beautify(nunjucks.renderString(content,
-    {...entities[page], css: classNames}), beautifyConfig)
+  const builded = beautify(
+    nunjucks.renderString(content, {
+      ...entities[page],
+      css: classNames,
+      __common__: entities.__common__,
+      env: process.env,
+    }),
+    beautifyConfig,
+  )
   return {file, content: builded}
 }
